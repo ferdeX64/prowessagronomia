8989e81a4e9c553527f36aabf3d4c5a6277722bb
khrizLlano <criss0498@yahoo.es>
2022-01-13 17:20:55 -0500

    Actualizacion de Pagina secundaria

---
+++ lib/widgets/pagina_secundaria.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class PaginaSecundariaWidget extends StatelessWidget {
+  const PaginaSecundariaWidget
+({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 7.0, vertical: 14.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            /*const Icon(Icons.cable, size: 50.0),
+            Text('Materiales', style: Theme.of(context).textTheme.headline4),*/
+          ],
+        ),
+      ),
+    );
+  }
+}

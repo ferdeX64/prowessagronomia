504d03eb56ff7c50fcaefaff985dec966370acba
khrizLlano <criss0498@yahoo.es>
2022-01-14 16:25:48 -0500

    Ccarpeta src

---
+++ lib/src/pages/homepage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  static String id = 'homepage';
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(),
+    );
+  }
+}

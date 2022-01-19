cce233a9839d9c50feca0e762151e5b2e888b299
Erick Fernández <eafernandez3@espe.edu.ec>
2022-01-19 17:21:44 -0500

    Slide menu

--- lib/src/widgets/widget_drawer.dart
+++ lib/src/widgets/widget_drawer.dart
@@ -1,49 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:prowessagronomia/src/pages/tiendas_page.dart';
 
 class MenuLateral extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child:Ink( color: Colors.green[600], child:  ListView(padding: EdgeInsets.zero, children: [
-        DrawerHeader(
-            decoration: const BoxDecoration(
-              color: Colors.lightGreenAccent,
+      child:Ink( 
+        color: Colors.green[600], 
+        child:  ListView(padding: EdgeInsets.zero, children: [
+          DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Colors.lightGreenAccent,
+              ),
+              child: Column(children: [
+                Expanded(
+                  child: Image.asset('assets/images/Logo_ProwessAgronomia.png'),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+              ])),
+          ListTile(
+              leading: const Icon(Icons.account_balance_outlined),
+              title: const Text('Inicio'),
+              tileColor: Colors.green[600],
+              onTap: () {}),
+          ListTile(
+              leading: const Icon(Icons.apartment_outlined),
+              title: const Text('Nosotros'),
+              tileColor: Colors.green[600],
+              onTap: () {}),
+          ListTile(
+              leading: const Icon(Icons.badge_outlined),
+              title: const Text('Equipo'),
+              tileColor: Colors.green[600],
+              onTap: () {}),
+          ListTile(
+              leading: const Icon(Icons.shopping_bag_outlined),
+              title: const Text('MarketPlace'),
+              tileColor: Colors.green[600],
+              onTap: () {
+                Navigator.push<void>(
+            context,
+            MaterialPageRoute<void>(
+              builder: (BuildContext context) =>
+                  const StorePage(),
             ),
-            child: Column(children: [
-              Expanded(
-                child: Image.asset('assets/images/Logo_ProwessAgronomia.png'),
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-            ])),
-        ListTile(
-            leading: const Icon(Icons.account_balance_outlined),
-            title: const Text('Inicio'),
-            tileColor: Colors.green[600],
-            onTap: () {}),
-        ListTile(
-            leading: const Icon(Icons.apartment_outlined),
-            title: const Text('Nosotros'),
-            tileColor: Colors.green[600],
-            onTap: () {}),
-        ListTile(
-            leading: const Icon(Icons.badge_outlined),
-            title: const Text('Equipo'),
-            tileColor: Colors.green[600],
-            onTap: () {}),
-        ListTile(
-            leading: const Icon(Icons.shopping_bag_outlined),
-            title: const Text('MarketPlace'),
-            tileColor: Colors.green[600],
-            onTap: () {}),
-        ListTile(
-            leading: const Icon(Icons.account_box_rounded),
-            title: const Text('Mi Cuenta'),
-            tileColor: Colors.green[600],
-            onTap: () {}),
-      ]),
-    )
+          );
+              }),
+          ListTile(
+              leading: const Icon(Icons.account_box_rounded),
+              title: const Text('Mi Cuenta'),
+              tileColor: Colors.green[600],
+              onTap: () {}),
+        ]),
+     )
     );
   }
 }

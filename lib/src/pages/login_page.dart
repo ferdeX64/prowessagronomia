6d514730b839b92eb4db030bf2084f1b25dc1400
khrizLlano <criss0498@yahoo.es>
2022-01-25 17:37:28 -0500

    Pagina Login

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -12,7 +12,7 @@ class _LoginPageState extends State<LoginPage> {
    @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
+      builder: (BuildContext context, BoxConstraints constraints) {
       return SafeArea(
         left: true,
         right: true,
@@ -23,13 +23,28 @@ class _LoginPageState extends State<LoginPage> {
           backgroundColor: Colors.orange[100],
           body: Stack(
             children: [
+              const Positioned(
+                left: 125,
+                top: 100,
+                child: Text("Iniciar Sesión",
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30,
+                    color: Colors.black
+                  )
+                ),
+              ),
               Positioned(
                 left: 15,
                 right: 15,
                 bottom: 440,
                 child: Container(
                     height: 200,
-                    decoration: BoxDecoration(image: DecorationImage(image: AssetImage("Assets/rent.png"))),
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/Logo_ProwessAgronomia.png")
+                      )
+                    ),
                   ),
               ),
               const Positioned(
@@ -39,14 +54,15 @@ class _LoginPageState extends State<LoginPage> {
                 child: TextField(
                   textAlign: TextAlign.start,
                   decoration: InputDecoration(
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black),
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black),
-                      ),
-                      hintText: "Mail@example.com",
-                      labelStyle: TextStyle(color: Color(0xFF424242))),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    hintText: "Correo@example.com",
+                    labelStyle: TextStyle(color: Color(0xFF424242))
+                  ),
                 ),
               ),
               const Positioned(
@@ -66,13 +82,13 @@ class _LoginPageState extends State<LoginPage> {
                       focusedBorder: UnderlineInputBorder(
                         borderSide: BorderSide(color: Colors.black),
                       ),
-                      hintText: "Password",
-                      labelStyle: TextStyle(color: Color(0xFF424242))),
+                      hintText: "Contraseña",
+                      labelStyle: TextStyle(color: Color(0xFF424242))
+                  ),
                 ),
               ),
               Positioned(
-                  left: 0,
-                  right: 215,
+                  left: 115,
                   bottom: 170,
                   child: TextButton(
                       style: TextButton.styleFrom(
@@ -81,28 +97,47 @@ class _LoginPageState extends State<LoginPage> {
                       ),
                       onPressed: () {},
                       child: const Text(
-                        "Forget Password?",
+                        "Olvidaste Tu Contraseña?",
                         style: TextStyle(fontSize: 14, color: Colors.black),
-                      ))),
+                      )
+                  )
+              ),
               Positioned(
-                  left: 15,
-                  right: 15,
-                  bottom: 110,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(),
-                      borderRadius: BorderRadius.circular(15.0),
+                left: 15,
+                right: 15,
+                bottom: 110,
+                child: Container(
+                  width: 50,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2.5),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: TextButton(
+                    style: const ButtonStyle(),
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/home");
+                    },
+                    child: const Text(
+                      "Siguiente",style: TextStyle(color: Colors.black),
                     ),
-                    child: TextButton(
-                      style: const ButtonStyle(),
-                      onPressed: () {
-                        Navigator.pushNamed(context, "/home");
-                      },
-                      child: Text(
-                        "LOGIN",style: TextStyle(color: Colors.black),
+                  ),
+                )
+              ),
+              Positioned(
+                  left: 125,
+                  bottom: 25,
+                  child: TextButton(
+                      style: TextButton.styleFrom(
+                        elevation: 0,
+                        primary: Colors.black,
                       ),
-                    ),
-                  )),
+                      onPressed: () {},
+                      child: const Text(
+                        "Eres nuevo? Registrate",
+                        style: TextStyle(fontSize: 14, color: Colors.black),
+                      )
+                  )
+              ),
             ],
           ),
         ),

acb7e1b9fce911c5cf339eeebb115a288deb5113
khrizLlano <criss0498@yahoo.es>
2022-02-02 17:02:39 -0500

    Pagina Login version 0.1

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -1,6 +1,8 @@
-// ignore_for_file: deprecated_member_use
-
+import 'dart:ui';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nb_utils/nb_utils.dart';
 import 'package:prowessagronomia/src/pages/sign_up.dart';
 
 class LoginPage extends StatefulWidget {
@@ -11,15 +13,222 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    var size = MediaQuery.of(context).size;
-    var width = size.width;
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: ScrollConfiguration(
+        behavior: MyBehavior(),
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: size.height,
+            child: Stack(
+              children: [
+                SizedBox(
+                  height: size.height,
+                  child: Image.asset(
+                    'assets/images/Logo_ProwessAgronomia.png',
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      const Expanded(
+                        child: SizedBox(),
+                      ),
+                      Expanded(
+                        flex: 7,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(30),
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaY: 25, sigmaX: 25),
+                            child: SizedBox(
+                              width: size.width * .9,
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(
+                                      top: size.width * .15,
+                                      bottom: size.width * .1,
+                                    ),
+                                    child: const Text(
+                                      'Iniciar Sesión',
+                                      style: TextStyle(
+                                        fontSize: 35,
+                                        fontWeight: FontWeight.w600,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                  component(
+                                    Icons.account_circle_outlined,
+                                    'Usuario...',
+                                    false,
+                                    false,
+                                  ),
+                                  component(
+                                    Icons.email_outlined,
+                                    'Correo...',
+                                    false,
+                                    true,
+                                  ),
+                                  component(
+                                    Icons.lock_outline,
+                                    'Contraseña...',
+                                    true,
+                                    false,
+                                  ),
+                                  
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      RichText(
+                                        text: TextSpan(
+                                          text: '¿Olvidaste tu contraseña?',
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                          recognizer: TapGestureRecognizer()
+                                            ..onTap = () {
+                                              HapticFeedback.lightImpact();
+                                              Fluttertoast.showToast(
+                                                msg:'¿Olvidaste tu contraseña? boton presionado',
+                                              );
+                                            },
+                                        ),
+                                      ),
+                                      RichText(
+                                        text: TextSpan(
+                                          text: 'Crear una nueva cuenta',
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                          recognizer: TapGestureRecognizer()
+                                          ..onTap = () {
+                                            HapticFeedback.lightImpact();
+                                            Fluttertoast.showToast(
+                                              msg:
+                                                  'Ingrese sus datos para crear la Cuenta',
+                                            );
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                builder: (BuildContext context) => const SingUpScreen()
+                                              )
+                                            );
+                                          },
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: size.width * .3),
+                                  InkWell(
+                                    splashColor: Colors.transparent,
+                                    highlightColor: Colors.transparent,
+                                    onTap: () {
+                                      HapticFeedback.lightImpact();
+                                      Fluttertoast.showToast(
+                                        msg: 'Sign-In button pressed',
+                                      );
+                                    },
+                                    child: Container(
+                                      margin: EdgeInsets.only(
+                                        bottom: size.width * .05,
+                                      ),
+                                      height: size.width / 8,
+                                      width: size.width / 1.25,
+                                      alignment: Alignment.center,
+                                      decoration: BoxDecoration(
+                                        color: Colors.black.withOpacity(.1),
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                      child: const Text(
+                                        'Ingresar',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const Expanded(
+                        child: SizedBox(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget component(IconData icon,String hintText,bool isPassword,bool isEmail){
+    Size size = MediaQuery.of(context).size;
     return Container(
-          
-        );
-      
+      height: size.width / 8,
+      width: size.width / 1.25,
+      alignment: Alignment.center,
+      padding: EdgeInsets.only(right: size.width / 30),
+      decoration: BoxDecoration(
+        color: Colors.black.withOpacity(.1),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        style: const TextStyle(
+          color: Colors.black,
+        ),
+        obscureText: isPassword,
+        keyboardType: isEmail ? TextInputType.emailAddress : TextInputType.text,
+        decoration: InputDecoration(
+          prefixIcon: Icon(
+            icon,
+            color: Colors.black,
+          ),
+          border: InputBorder.none,
+          hintMaxLines: 1,
+          hintText: hintText,
+          hintStyle: const TextStyle(
+            fontSize: 14,
+            color: Colors.black,
+          ),
+          suffixIcon: IconButton(
+            onPressed: () {
+              isPassword = !isPassword;
+              setState(() {});
+            },
+            icon: Icon(
+              isPassword
+                  ? Icons.visibility
+                  : Icons.visibility_off,
+            )
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+    BuildContext context,
+    Widget child,
+    AxisDirection axisDirection,
+  ) {
+    return child;
   }
 }

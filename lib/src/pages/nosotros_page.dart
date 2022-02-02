8d362e1a4679eacc9cbfeab6dda22b3ff8f60d70
Erick Fernández <eafernandez3@espe.edu.ec>
2022-02-02 17:36:30 -0500

    Carrito page

---
+++ lib/src/pages/nosotros_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class NosotrosPage extends StatelessWidget {
+  const NosotrosPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
